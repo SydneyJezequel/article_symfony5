458ff86f54e46b43185d32c66a26b6890755051d
Sydney Jezequel <107682562+SydneyJezequel@users.noreply.github.com>
2022-08-29 17:08:56 +0200

    maj premier projet Symfony

--- public/index.php
+++ public/index.php
@@ -2,8 +2,10 @@
 
 use App\Kernel;
 
+// On fait un autoload
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+// On appelle une fonction qui va construire mon Kernel (coeur de l'application) : Gère le Controller.
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
